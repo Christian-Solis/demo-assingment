ad67c251d4ca311283f4dd7a2890efb5bb63c6b8
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-02-13 12:48:46 -0600

    Demo
    Demo finished

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------------------
+# Christian Ricardo Solís Cortés
+# A01063685
+# Artificial Intelligence
+# Demo Assignment for AlphaGrader:
+# Build a calculator that adds two numbers.
+# -----------------------------------------------------------------------------
+
+a = input('Number 1: ')
+b = input('Number 2: ')
+
+c = int(a) + int(b)
+
+print(c)
